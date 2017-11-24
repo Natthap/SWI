b7363755d0c34e6e3be8f5663be52a509bb779b8
DESKTOP-KGE1PST\mzikl <martin.zikla@eea.sk>
2017-11-24 22:24:31 +0100

    initial commit

---
+++ src/main/java/basicfolder/main.java
@@ -0,0 +1,13 @@
+package basicfolder;
+
+/**
+ * Created by mzikl on 24.11.2017.
+ */
+public class main {
+
+    public void pozdrav() {
+        System.out.println("Ahoj, svet ja som programator!");
+    }
+
+
+}
