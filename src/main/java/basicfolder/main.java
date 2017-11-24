ae3102bbc09f2d158c69d3003d834ca298e7c886
DESKTOP-KGE1PST\mzikl <martin.zikla@eea.sk>
2017-11-24 23:00:55 +0100

    pocitanie pridane

--- src/main/java/basicfolder/main.java
+++ src/main/java/basicfolder/main.java
@@ -7,10 +7,19 @@ public class main {
 
     public void hlavnaMetoda() {
         pozdrav();
+        pocitanie();
     }
 
     private void pozdrav() {
         System.out.println("Ahoj svet, ja som programator!");
+    }
+
+    private void pocitanie() {
+        int sucet = 0;
+        for(int a = 1; a < 20; a++) {
+            sucet += a;
+        }
+        System.out.print(sucet);
     }
 
 }
